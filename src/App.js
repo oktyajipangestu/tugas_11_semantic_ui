d011fb193a382d548d9a7bb104893eb02186315e
oktyajipangestu <oktyajipangestu@gmail.com>
2021-07-04 20:43:28 +0700

    tugas 11 selesai

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ import {
   Icon,
   Search,
   Button,
+  Placeholder,
+  List,
+  Dimmer,
+  Loader,
+  Image,
 } from "semantic-ui-react";
 
 class App extends Component {
@@ -38,6 +43,75 @@ class App extends Component {
             </Grid.Row>
           </Grid>
         </Segment>
+        <Segment>
+          <Dimmer active>
+            <Loader>Loading</Loader>
+          </Dimmer>
+
+          <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
+        </Segment>
+
+        <Grid columns={3} stackable>
+          <Grid.Column width={6}>
+            <Segment raised>
+              <Placeholder>
+                <Placeholder.Header image>
+                  <Placeholder.Line />
+                  <Placeholder.Line />
+                </Placeholder.Header>
+                <Placeholder.Paragraph>
+                  <Placeholder.Line length="medium" />
+                  <Placeholder.Line length="short" />
+                </Placeholder.Paragraph>
+              </Placeholder>
+            </Segment>
+          </Grid.Column>
+
+          <Grid.Column width={6}>
+            <Segment raised>
+              <Placeholder>
+                <Placeholder.Header image>
+                  <Placeholder.Line />
+                  <Placeholder.Line />
+                </Placeholder.Header>
+                <Placeholder.Paragraph>
+                  <Placeholder.Line length="medium" />
+                  <Placeholder.Line length="short" />
+                </Placeholder.Paragraph>
+              </Placeholder>
+            </Segment>
+          </Grid.Column>
+
+          <Grid.Column width={4}>
+            <Segment raised>
+              <h3>Website Terkait</h3>
+              <List>
+                <List.Item
+                  icon="linkify"
+                  content={<a href="http://www.google.com">Google</a>}
+                />
+                <List.Item
+                  icon="linkify"
+                  content={<a href="http://www.facebook.com">Facebook</a>}
+                />
+                <List.Item
+                  icon="linkify"
+                  content={
+                    <a href="http://www.semantic-ui.com">semantic-ui.com</a>
+                  }
+                />
+                <List.Item
+                  icon="linkify"
+                  content={<a href="http://www.niomic.com">Niomic</a>}
+                />
+                <List.Item
+                  icon="linkify"
+                  content={<a href="https://reactjs.org/">React</a>}
+                />
+              </List>
+            </Segment>
+          </Grid.Column>
+        </Grid>
       </div>
     );
   }
